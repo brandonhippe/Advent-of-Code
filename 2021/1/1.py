a408eb90db195cc554beaf554e2d8fc7fe554774
brandonhippe <runhippe@gmail.com>
2021-11-30 23:02:14 -0800

    2021 Day 1 completed

--- 2021/1/1.py
+++ 2021/1/1.py
@@ -5,7 +5,7 @@ def main():
     for (i, line) in enumerate(lines):
         lines[i] = int(line)
 
-    message = "Part 1: "
+    print("Part 1:")
     increased = 0
     for i in range(1, len(lines)):
         if lines[i] > lines[i - 1]:
@@ -13,4 +13,18 @@ def main():
 
     print("Number of increases = " + str(increased))
 
+    print("Part 2:")
+    increased = 0
+    for i in range(3, len(lines)):
+        sums = [0] * 2
+        frame1 = lines[i - 3:i]
+        frame2 = lines[i - 2:i + 1]
+        for j in range(3):
+            sums[0] += frame1[j]
+            sums[1] += frame2[j]
+
+        if sums[1] > sums[0]:
+            increased += 1
+
+    print("Number of increases = " + str(increased))
 main()
