1c3eea8b558224ab2be4e1576cc364334fdec59c
brandonhippe <runhippe@gmail.com>
2021-12-09 21:21:34 -0800

    Day 8 Refactored

--- 2021/8/8.py
+++ 2021/8/8.py
@@ -1,158 +1,58 @@
-numSegs = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 dispPats = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
+occurences = {344: 'a', 204: 'b', 304: 'c', 266: 'd', 96: 'e', 396: 'f', 280: 'g'}
 
 class code:
     def __init__(self, pattern, outputs):
         self.digitReps = [-1] * 10
         self.digitPats = [-1] * 10
-        self.posLetters = ['abcdefg'] * 7
-        self.pattern = [''] * 10
-        self.outputs = [''] * 4
+        self.pattern = pattern[:]
+        self.outputs = outputs[:]        
+        self.pairs = {'a': '', 'b': '', 'c': '', 'd': '', 'e': '', 'f': '', 'g': ''}
 
-        for (j, p) in enumerate(pattern):
-            patternAscii = [0] * len(p)
-            for (i, l) in enumerate(p):
-                patternAscii[i] = ord(l)
-
-            patternAscii.sort()
-            for n in patternAscii:
-                self.pattern[j] += chr(n)
-
-        for (j, o) in enumerate(outputs):
-            outputsAscii = [0] * len(o)
-            for (i, l) in enumerate(o):
-                outputsAscii[i] = ord(l)
-
-            outputsAscii.sort()
-            for n in outputsAscii:
-                self.outputs[j] += chr(n)
+        for o in range(ord('a'), ord('g') + 1):
+            c = chr(o)
+            freq = 0
+            num = 0
+            for p in pattern:
+                inP = False
+                for test in p:
+                    if test == c:
+                        inP = True
+                        freq += 1
+                
+                if inP:
+                    num += len(p)
             
+            index = freq * num
+            self.pairs[c] = occurences[index]
+        
+        self.output = 0
+        for o in outputs:
+            self.output *= 10
+            self.output += dispPats.index(self.getStdOutput(o))
 
+    def getStdOutput(self, output):
+        new = ''
+        for o in output:
+            new += self.pairs[o]
 
-        for p in self.pattern:
-            if len(p) == 2 or len(p) == 3 or len(p) == 4 or len(p) == 7:
-                i = 0
-                index = self.hash(p, i)
-                while self.digitReps[index] != -1:
-                    i += 1
-                    index = self.hash(p, i)
+        return sortStr(new)
 
-                num = numSegs.index(len(p))
-                self.digitPats[index] = p
-                self.digitReps[index] = num
-                for l in dispPats[num]:
-                    self.updatePossible(l, p[:])
+        
 
-    def hash(self, digit, collison):
-        count = collison
-        for c in digit:
-            count += ord(c)
+    
 
-        return count % 10
+def sortStr(s):
+    ordArr = [0] * len(s)
+    for (i, l) in enumerate(s):
+        ordArr[i] = ord(l)
+    
+    ordArr.sort()
+    ret = ''
+    for o in ordArr:
+        ret += chr(o)
 
-    def letterIndex(self, letter):
-        return ord(letter) - ord('a')
-
-    def updatePossible(self, letter, pat):
-        index = self.letterIndex(letter)
-        for l in self.posLetters[index]:
-            if l not in pat:
-                self.posLetters[index] = self.removeChar(self.posLetters[index], l)
-
-    def removeChar(self, letters, l):
-        try:
-            i = letters.index(l)
-        except ValueError:
-            return letters
-        temp = letters[:i]
-        temp = temp + letters[i + 1:]
-        return temp
-
-
-    def genOutput(self):
-        while True:
-            # Update Possible Letters (Finds the top segment and 3 pairs)
-            prev = self.posLetters[:]
-
-            for pos in self.posLetters:
-                if self.posLetters.count(pos) == len(pos):
-                    for (i, remove) in enumerate(self.posLetters):
-                        if remove != pos:
-                            for l in pos:
-                                self.posLetters[i] = self.removeChar(self.posLetters[i], l)
-
-            if prev == self.posLetters:
-                break
-
-        # Work cases by length (3 length 5, 3 length 6)
-        for p in self.pattern:
-            if not (len(p) == 5 or len(p) == 6):
-                continue
-
-            i = 0
-            index = self.hash(p, i)
-            while self.digitReps[index] != -1:
-                i += 1
-                index = self.hash(p, i)
-
-            if len(p) == 5:
-                # 2 is only case to use 'eg' pair, 5 is only case to use 'bd', 3 only uses one of each
-                eg = self.posLetters[self.letterIndex('e')][:]
-                bd = self.posLetters[self.letterIndex('b')][:]
-                egIn = True
-                bdIn = True
-
-                for i in range(2):
-                    if eg[i] not in p:
-                        egIn = False
-                    if bd[i] not in p:
-                        bdIn = False
-
-                if egIn:
-                    num = 2
-                elif bdIn:
-                    num = 5
-                else:
-                    num = 3
-            elif len(p) == 6:
-                # 0 is missing 'bd' pair, 6 is missing 'cf' pair, 9 is missing 'eg' pair
-                eg = self.posLetters[self.letterIndex('e')][:]
-                cf = self.posLetters[self.letterIndex('c')][:]
-                bd = self.posLetters[self.letterIndex('b')][:]
-                egIn = True
-                cfIn = True
-                bdIn = True
-
-                for i in range(2):
-                    if eg[i] not in p:
-                        egIn = False
-                    if cf[i] not in p:
-                        cfIn = False
-                    if bd[i] not in p:
-                        bdIn = False
-
-                if not bdIn:
-                    num = 0
-                elif not cfIn:
-                    num = 6
-                else:
-                    num = 9
-
-            self.digitPats[index] = p
-            self.digitReps[index] = num
-
-        count = 0
-        for o in self.outputs:
-            i = 0
-            index = self.hash(o, i)
-            while not self.digitPats[index] == o:
-                i += 1
-                index = self.hash(o, i)
-
-            count *= 10
-            count += self.digitReps[index]
-
-        return count
+    return ret
 
 def main():
     with open('input.txt',encoding='UTF-8') as f:
@@ -190,7 +90,7 @@ def main():
     count = 0
     for i in range(len(patterns)):
         c = code(patterns[i], outputs[i])
-        count += c.genOutput()
+        count += c.output
 
     print("Sum of output values: " + str(count))
 
