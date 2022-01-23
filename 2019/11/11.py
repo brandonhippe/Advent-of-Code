f6e09649aa6460b609f3455daf34f7dcec1ea691
brandonhippe <bhippe@pdx.edu>
2022-01-23 14:27:52 -0800

    2019 Day 11 Finished

---
+++ 2019/11/11.py
@@ -0,0 +1,268 @@
+def runCode(state, inputs):
+    # Modes: 0: Position, 1: Immediate, 2: Relative
+    data, i, relBase, outputs = state
+    
+    while True:
+        line = data[i] if i in data else 0
+
+        if line == 99:
+            # HLT
+            break
+
+        opCode = line % 100
+        modes = [int(x) for x in str(line // 100)]
+        operands = []
+
+        for j in range(i + 1, i + 4):
+            operands.append(data[j] if j in data else 0)
+
+        if opCode == 1:
+            # ADD
+            value = 0
+            for op in operands[:-1]:
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    value += data[op] if op in data else 0
+                elif mode == 1:
+                    value += op
+                elif mode == 2:
+                    value += data[relBase + op] if relBase + op in data else 0
+
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+            
+            if mode == 0:
+                data[operands[-1]] = value
+            elif mode == 2:
+                data[relBase + operands[-1]] = value
+            
+            i += 4
+        elif opCode == 2:
+            # MULT
+            value = 1
+            for op in operands[:-1]:
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    value *= data[op] if op in data else 0
+                elif mode == 1:
+                    value *= op
+                elif mode == 2:
+                    value *= data[relBase + op] if relBase + op in data else 0
+
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                data[operands[-1]] = value
+            elif mode == 2:
+                data[relBase + operands[-1]] = value
+
+            i += 4
+        elif opCode == 3:
+            # STR
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if len(inputs) == 0:
+                return [False, [data, i, relBase, outputs]]
+            else:
+                if mode == 0:          
+                    data[operands[0]] = inputs.pop(0)
+                elif mode == 2:
+                    data[relBase + operands[0]] = inputs.pop(0)
+
+            i += 2
+        elif opCode == 4:
+            # OUT
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+            
+            if mode == 0:
+                outputs.append(data[operands[0]] if operands[0] in data else 0)
+            elif mode == 1:
+                outputs.append(operands[0])
+            elif mode == 2:
+                outputs.append(data[relBase + operands[0]] if relBase + operands[0] in data else 0)
+
+            i += 2
+        elif opCode == 5:
+            # JNZ
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                value = data[operands[0]] if operands[0] in data else 0
+            elif mode == 1:
+                value = operands[0]
+            elif mode == 2:
+                value = data[relBase + operands[0]] if relBase + operands[0] in data else 0
+
+            if value != 0:
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    i = data[operands[1]] if operands[1] in data else 0
+                elif mode == 1:
+                    i = operands[1]
+                elif mode == 2:
+                    i = data[relBase + operands[1]] if relBase + operands[1] in data else 0
+            else:
+                i += 3
+        elif opCode == 6:
+            # JZ
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                value = data[operands[0]] if operands[0] in data else 0
+            elif mode == 1:
+                value = operands[0]
+            elif mode == 2:
+                value = data[relBase + operands[0]] if relBase + operands[0] in data else 0
+
+            if value == 0:
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    i = data[operands[1]] if operands[1] in data else 0
+                elif mode == 1:
+                    i = operands[1]
+                elif mode == 2:
+                    i = data[relBase + operands[1]] if relBase + operands[1] in data else 0
+            else:
+                i += 3
+        elif opCode == 7:
+            # LT
+            value = 0
+            for (j, op) in enumerate(operands[:-1]):
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    value += (data[op] if op in data else 0) * ((-1) ** j)
+                elif mode == 1:
+                    value += op * ((-1) ** j)
+                elif mode == 2:
+                    value += (data[relBase + op] if relBase + op in data else 0) * ((-1) ** j)
+
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                data[operands[-1]] = int(value < 0)
+            elif mode == 2:
+                data[relBase + operands[-1]] = int(value < 0)
+            
+            i += 4
+        elif opCode == 8:
+            # EQ
+            value = 0
+            for (j, op) in enumerate(operands[:-1]):
+                mode = modes.pop(-1) if len(modes) != 0 else 0
+
+                if mode == 0:
+                    value += (data[op] if op in data else 0) * ((-1) ** j)
+                elif mode == 1:
+                    value += op * ((-1) ** j)
+                elif mode == 2:
+                    value += (data[relBase + op] if relBase + op in data else 0) * ((-1) ** j)
+
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                data[operands[-1]] = int(value == 0)
+            elif mode == 2:
+                data[relBase + operands[-1]] = int(value == 0)
+            
+            i += 4
+        elif opCode == 9:
+            # REL BASE
+            mode = modes.pop(-1) if len(modes) != 0 else 0
+
+            if mode == 0:
+                relBase += data[operands[0]] if operands[0] in data else 0
+            elif mode == 1:
+                relBase += operands[0]
+            elif mode == 2:
+                relBase += data[relBase + operands[0]] if relBase + operands[0] else 0
+
+            i += 2
+
+    return [True, outputs]
+
+def handler(code, start):
+    state = [code, 0, 0, []]
+    inputs = start
+
+    loc = [0, 0]
+    lStr = arrToStr(loc)
+    facing = [0, 1]
+
+    panels = {}
+    while True:
+        result = runCode(state, inputs)
+
+        if result[0]:
+            return panels
+        else:
+            state = result[1]
+            outputs = state.pop(-1)
+            state.append([])
+
+            if len(outputs) == 0:
+                inputs = [panels[lStr] if lStr in panels else 0]
+            else:
+                panels[lStr] = outputs[0]
+
+                if outputs[1] == 0:
+                    # ROTATE LEFT
+                    facing = [-facing[1], facing[0]]
+                else:
+                    # ROTATE RIGHT
+                    facing = [facing[1], -facing[0]]
+
+                for (i, (item1, item2)) in enumerate(zip(loc, facing)):
+                    loc[i] = item1 + item2
+
+                lStr = arrToStr(loc)
+
+def arrToStr(arr):
+    string = str(arr[0])
+    for a in arr[1:]:
+        string += ',' + str(a)
+    return string
+
+def strToArr(string):
+    return [int(x) for x in string.split(',')]
+
+def main():
+    with open('input.txt', encoding='UTF-8') as f:
+        lines = [int(l) for l in f.readline().strip().split(',')]
+
+    code = {}
+    for (i, x) in enumerate(lines):
+        code[i] = x
+
+    print(f"\nPart 1:\nNumber of panels painted at least once: {len(handler(code, [0]))}")
+
+    print("\nPart 2:\nRegistration Identifier:\n")
+    panels = handler(code, [1])
+
+    mins = [float('inf')] * 2
+    maxs = [float('-inf')] * 2
+    for l in panels:
+        if panels[l] == 0:
+            continue
+        
+        loc = strToArr(l)
+
+        for (i, c) in enumerate(loc):
+            if c < mins[i]:
+                mins[i] = c
+
+            if c > maxs[i]:
+                maxs[i] = c
+
+    for y in range(maxs[1], mins[1] - 1, -1):
+        for x in range(mins[0], maxs[0] + 1,):
+            c = panels[arrToStr([x, y])] if arrToStr([x, y]) in panels else 0
+            c = '#' if c == 1 else ' '
+            print(c, end='')
+
+        print('')
+
+main()
