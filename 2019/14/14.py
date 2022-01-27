5c97b67ccc499a20453b3c772f185005ca078a95
brandonhippe <bhippe@pdx.edu>
2022-01-26 16:02:21 -0800

    2019 Day 14 Finished

---
+++ 2019/14/14.py
@@ -0,0 +1,93 @@
+import math
+
+class reaction:
+    def __init__(self, line):
+        line = line.split(' => ')
+        for (i, l) in enumerate(line):
+            line[i] = l.split(',')
+
+        self.inputs = {}
+        self.outputs = {}
+
+        for inp in line[0]:
+            inp = inp.strip()
+            inp = inp.split(' ')
+            self.inputs[inp[1]] = int(inp[0])
+
+        for out in line[1]:
+            out = out.split(' ')
+            self.outputs[out[1]] = int(out[0])
+
+def oreNeeded(chemicals, create, amount, extras=None):
+    # Calculates # of ore needed to make amount of create using chemicals
+    if create == 'ORE':
+        return [amount, extras]
+
+    if extras == None:
+        extras = {}
+
+    r = chemicals[create]
+
+    reactionNum = math.ceil(amount / r.outputs[create])
+
+    total = 0
+    for i in r.inputs:
+        needed = r.inputs[i] * reactionNum
+        if i in extras:
+            needed -= extras[i]
+            extras[i] = -needed if needed < 0 else 0
+
+        if needed > 0:
+            ore, extras = oreNeeded(chemicals, i, needed, extras)
+            total += ore
+
+    if create in extras:
+        extras[create] += (r.outputs[create]) * reactionNum - amount
+    else:
+        extras[create] = (r.outputs[create]) * reactionNum - amount
+
+    return [total, extras]
+
+def main():
+    with open('input.txt', encoding='UTF-8') as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    reactions = []
+    chemicals = {}
+    for line in lines:
+        reactions.append(reaction(line))
+
+    for r in reactions:
+        for o in r.outputs:
+            chemicals[o] = r
+
+    oneFuel = oreNeeded(chemicals, 'FUEL', 1)[0]
+    print(f"\nPart 1:\nAmount of ore needed to make 1 fuel: {oneFuel}")
+
+    ore = 1000000000000
+    fuelMade = 0
+
+    while True:
+        fuelToMake = ore // oneFuel
+        
+        while True:
+            oreUsed, extras = oreNeeded(chemicals, 'FUEL', fuelToMake)
+
+            extrasSum = sum(extras.values())
+            if oreUsed >= ore or extrasSum == 0:
+                continuing = extrasSum == 0
+                break
+
+            fuelToMake += ((ore - oreUsed) // oneFuel) + 1
+
+        if not continuing:
+            fuelMade += fuelToMake - 1
+            break
+
+        amt = ore // oreUsed
+        fuelMade += fuelToMake * amt
+        ore -= oreUsed * amt
+
+    print(f"\nPart 2:\nAmount of fuel made with one trillion ore: {fuelMade}")
+
+main()
