a4ed5e6289cc5710521a2954289505dea355bbdb
brandonhippe <bhippe@pdx.edu>
2022-04-12 14:59:54 -0700

    2018 Day 15 Finished

--- 2018/15/15.py
+++ 2018/15/15.py
@@ -1,6 +1,6 @@
 import time
 import re
-import heapq
+import copy
 
 READING_ORDER = [[0, -1], [-1, 0], [1, 0], [0, 1]]
 
@@ -36,31 +36,32 @@ def findNearest(unit, allies, enemies, walls):
     inQueue = [Path([unit.pos])]
 
     while len(inQueue) != 0:
-        currPath = heapq.heappop(inQueue)
+        currPath = inQueue.pop(0)
+        
         if currPath.pos in [e.pos for e in enemies]:
             return [enemies[[e.pos for e in enemies].index(currPath.pos)], currPath.path[1]]
 
         for n in [[p + o for p, o in zip(currPath.pos, offset)] for offset in READING_ORDER]:
-            if n in [v.pos for v in visited] or n in walls or n in [a.pos for a in allies]:
+            if n in visited or n in walls or n in [q.pos for q in inQueue] or n in [a.pos for a in allies]:
                 continue
 
-            heapq.heappush(inQueue, Path(currPath.path + [n]))
-            
-        visited.append(currPath)
+            inQueue.append(Path(currPath.path + [n]))
+
+        visited.append(currPath.pos)
 
     return [None] * 2
 
-def iterate(walls, elfs, goblins):
+def iterate(walls, elves, goblins):
     complete = True
-    for x, y in [u.pos for u in sorted(elfs + goblins)]:            
-        if [x, y] in [e.pos for e in elfs]:
-            unit = elfs[[e.pos for e in elfs].index([x, y])]
-            allies = elfs
+    for x, y in [u.pos for u in sorted(elves + goblins)]:            
+        if [x, y] in [e.pos for e in elves]:
+            unit = elves[[e.pos for e in elves].index([x, y])]
+            allies = elves
             enemies = goblins
         elif [x, y] in [g.pos for g in goblins]:
             unit = goblins[[g.pos for g in goblins].index([x, y])]
             allies = goblins
-            enemies = elfs
+            enemies = elves
         else:
             continue
 
@@ -93,16 +94,16 @@ def iterate(walls, elfs, goblins):
 
     return complete
             
-def printBattle(maxs, walls, elfs, goblins):
+def printBattle(maxs, walls, elves, goblins):
     for y in range(0, maxs[1] + 1):
         postStr = '   '
         for x in range(0, maxs[0] + 1):
             c = '.'
             if [x, y] in walls:
                 c = '#'
-            elif [x, y] in [e.pos for e in elfs]:
+            elif [x, y] in [e.pos for e in elves]:
                 c = 'E'
-                postStr += f'E({elfs[[e.pos for e in elfs].index([x, y])].hp}), '
+                postStr += f'E({elves[[e.pos for e in elves].index([x, y])].hp}), '
             elif [x, y] in [g.pos for g in goblins]:
                 c = 'G'
                 postStr += f'G({goblins[[g.pos for g in goblins].index([x, y])].hp}), '
@@ -116,7 +117,7 @@ def main(filename):
         lines = [line.strip('\n') for line in f.readlines()]
     
     walls = []
-    elfs = []
+    elves = []
     goblins = []
     maxs = [0, 0]
     for y, line in enumerate(lines):
@@ -125,30 +126,53 @@ def main(filename):
         for x, l in enumerate(line):
             if x > maxs[0]:
                 maxs[0] = x
+
             if l == '#':
                 walls.append([x, y])
             elif l == 'E':
-                elfs.append(Unit([x, y]))
+                elves.append(Unit([x, y]))
             elif l == 'G':
                 goblins.append(Unit([x, y]))
+
+    totalElves = len(elves)
+    
+    iteratingElves = copy.deepcopy(elves)
+    iteratingGoblins = copy.deepcopy(goblins)
     
     rounds = 0
-    while len(elfs) > 0 and len(goblins) > 0:
+    while len(iteratingElves) > 0 and len(iteratingGoblins) > 0:
         if testing:
             print(f"\nAfter {rounds} Round{'s' if rounds != 1 else ''}:")
-            printBattle(maxs, walls, elfs, goblins)
+            printBattle(maxs, walls, iteratingElves, iteratingGoblins)
 
-        if iterate(walls, elfs, goblins):
+        if iterate(walls, iteratingElves, iteratingGoblins):
             rounds += 1
 
     if testing:
         print(f"\nAfter {rounds} Round{'s' if rounds != 1 else ''}:")
-        printBattle(maxs, walls, elfs, goblins)
+        printBattle(maxs, walls, iteratingElves, iteratingGoblins)
 
-    winners = elfs[:] if len(goblins) == 0 else goblins[:]
-    print(f"\nPart 1:\nCombat Outcome: {rounds * sum(w.hp for w in winners)}")
+    winners = iteratingElves[:] if len(iteratingGoblins) == 0 else iteratingGoblins[:]
+    print(f"\nPart 1:\nCombat Outcome: {rounds} x {sum(w.hp for w in winners)} = {rounds * sum(w.hp for w in winners)}")
+
+    elfAttack = 3
+    while len(iteratingElves) < totalElves:
+        elfAttack += 1
+        iteratingElves = copy.deepcopy(elves)
+        iteratingGoblins = copy.deepcopy(goblins)
+        
+        for e in iteratingElves:
+            e.attack = elfAttack
+
+        rounds = 0
+        while len(iteratingElves) == totalElves and len(iteratingGoblins) > 0:
+            if iterate(walls, iteratingElves, iteratingGoblins):
+                rounds += 1
+
+    winners = iteratingElves[:] if len(iteratingGoblins) == 0 else iteratingGoblins[:]
+    print(f"\nPart 2:\nCombat Outcome: {rounds} x {sum(w.hp for w in winners)} = {rounds * sum(w.hp for w in winners)}")
 
 if __name__ == "__main__":
     init_time = time.perf_counter()
-    main("input1.txt")
+    main("input.txt")
     print(f"\nRan in {time.perf_counter() - init_time} seconds.")
