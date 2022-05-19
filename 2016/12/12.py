7e073d4687874b66a6392dd28a48306e0fc92659
brandonhippe <bhippe@pdx.edu>
2022-05-19 12:48:54 -0700

    2016 Day 12 Finished

---
+++ 2016/12/12.py
@@ -0,0 +1,75 @@
+import time
+import re
+from collections import defaultdict
+
+def cpy(regs, text):
+    x, y = text
+    if len(re.findall('-?\d+', x)) != 0:
+        x = int(x)
+    else:
+        x = regs[x]
+
+    regs[y] = x
+
+def inc(regs, x):
+    regs[x[0]] += 1
+
+def dec(regs, x):
+    regs[x[0]] -= 1
+
+def jnz(regs, text):
+    x, y = text
+    y = int(y)
+    if len(re.findall('-?\d+', x)) != 0:
+        x = int(x)
+    else:
+        x = regs[x]
+
+    if x != 0:
+        regs['PC'] += y - 1
+
+OPS = {"cpy": cpy, "inc": inc, "dec": dec, "jnz": jnz}
+
+def fib(n, memo):    
+    if n - 1 in memo:
+        tot = memo[n - 1]
+    else:
+        tot = fib(n - 1, memo)
+        memo[n - 1] = tot
+
+    return tot + memo[n - 2]
+
+def calc(regs, nums):
+    regs['a'] = fib(regs['d'] + 1, {0: 1, 1: 1})
+
+    return regs['a'] + nums[0] * nums[1]
+
+def main(filename):
+    with open(filename, encoding='UTF-8') as f:
+        instructions = [line.strip('\n').split(' ') for line in f.readlines()]
+    
+    regs = defaultdict(lambda: 0)
+    while 0 <= regs['PC'] < 9:
+        op, *text = instructions[regs['PC']]
+        OPS[op](regs, text)
+        regs['PC'] += 1
+
+    nums = [int(re.findall('-?\d+', l[1])[0]) for l in instructions[16:18]]
+
+    print(f"\nPart 1:\nValue in register a after program executes: {calc(regs, nums)}")
+
+    regs = defaultdict(lambda: 0)
+    regs['c'] = 1
+    while 0 <= regs['PC'] < 9:
+        op, *text = instructions[regs['PC']]
+        OPS[op](regs, text)
+        regs['PC'] += 1
+
+    nums = [int(re.findall('-?\d+', l[1])[0]) for l in instructions[16:18]]
+
+    print(f"\nPart 2:\nValue in register a after program executes: {calc(regs, nums)}")
+
+if __name__ == "__main__":
+    init_time = time.perf_counter()
+    main("input.txt")
+    print(f"\nRan in {time.perf_counter() - init_time} seconds.")
