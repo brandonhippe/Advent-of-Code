4015c0b67353272b134f866c90d0c684b54e2988
brandonhippe <bhippe@pdx.edu>
2022-07-11 14:31:03 -0700

    Day 2 in Python

--- 2020/2/2.py
+++ 2020/2/2.py
@@ -1,51 +1,28 @@
+from operator import xor
 import time
-
-class password:
-    def __init__(self, data):
-        self.min = int(data[0])
-        self.max = int(data[1])
-        self.letter = data[2].rstrip(data[2][-1])
-        self.pw = data[3].rstrip(data[3][-1])
-    
-    def validP1(self):
-        count = self.pw.count(self.letter)
-        return count >= self.min and count <= self.max
-    
-    def validP2(self):
-        return (self.pw[self.min - 1] == self.letter or self.pw[self.max - 1] == self.letter) and not (self.pw[self.min - 1] == self.letter and self.pw[self.max - 1] == self.letter)
-    
-def countValidP1(pwds):
-    count = 0
-    for pwd in pwds:
-        count += 1 if pwd.validP1() else 0
-    
-    return count
-
-def countValidP2(pwds):
-    count = 0
-    for pwd in pwds:
-        count += 1 if pwd.validP2() else 0
-
-    return count
+import re
 
 def main():
     with open('input.txt', encoding='UTF-8') as f:
-        lines = f.readlines()
+        lines = [line.strip('\n') for line in f.readlines()]
 
-    pwds = []
+    countP1, countP2 = 0, 0
+
     for line in lines:
-        data = line.split('-', 1)
-        temp = data[1].split(' ')
-        data.remove(data[1])
-        data.extend(temp)
+        counts = [int(x) for x in re.findall('\d+', line)]
+        c = re.findall('[a-z]:', line)[0][0]
+        pwd = re.split(' ', line)[-1]
 
-        pwd = password(data)
-        pwds.append(pwd)
+        if counts[0] <= len([a for a in pwd if a == c]) <= counts[1]:
+            countP1 += 1
+
+        if xor(pwd[counts[0] - 1] == c, pwd[counts[1] - 1] == c):
+            countP2 += 1
     
-    print('\nPart 1:\nValid Passwords: ' + str(countValidP1(pwds)))
+    print(f"\nPart 1:\nValid Passwords: {countP1}")
+    print(f"\nPart 2:\nValid Passwords: {countP2}")
 
-    print('\nPart 2:\nValid Passwords: ' + str(countValidP2(pwds)))
-
-init_time = time.perf_counter()
-main()
-print(f"\nRan in {time.perf_counter() - init_time} seconds")
+if __name__ == "__main__":
+    init_time = time.perf_counter()
+    main()
+    print(f"\nRan in {time.perf_counter() - init_time} seconds")
