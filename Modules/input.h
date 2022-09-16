e0f4dcc37d029c9fda3c7510bc8adeee6d2bc81b
brandonhippe <bhippe@pdx.edu>
2022-09-16 10:39:56 -0700

    Modified Vector and Input

--- Modules/input.h
+++ Modules/input.h
@@ -2,55 +2,95 @@
 #define INPUT_H
 
 
+// Include header files
 #include <stdlib.h>
 #include <stdio.h>
 #include "vector.h"
 
 
-struct vector *readInput(char *fileName);
+// Function Prototypes
+struct vector *multiLine(char *fileName);
+struct vector *singleLine(char *fileName, char *delim);
 
 
-void appendLine(struct vector *data, struct vector *line) {
+// Function to convert single dataline vector to a string
+char *getLine(struct vector *line) {
     int len = line->len;
     char *dataLine = (char*)calloc(len + 1, sizeof(char));
 
     for (int i = 0; i < len; i++) {
-        char c = *(char*)pop(line, 0);
-        if (c != -1) {
-            dataLine[i] = c;
-        }
+        dataLine[i] = *((char*)line->arr[i]);
     }
     
-    append(data, dataLine);
+    return dataLine;
 }
 
 
-struct vector *readInput(char *fileName) {
-    struct vector *data = createVector();
+// Function to read multi-line input from a file
+struct vector *multiLine(char *fileName) {
+    struct vector *data = createVector(stringsize, copyElement);
 
     FILE *fp = fopen(fileName, "r");
     if (!fp) {
         return NULL;
     }
 
-    struct vector *line = createVector();
+    struct vector *line = createVector(charsize, copyElement);
 
     while (!feof(fp)) {
         char *c = (char*)calloc(2, sizeof(char));
         *c = fgetc(fp);
 
         if (*c == '\n') {
-            appendLine(data, line);
-        } else {
-            append(line, c);
+            char *l = getLine(line);
+            appendVector(data, l);
+            deleteVector(line, false);
+            line = createVector(charsize, copyElement);
+        } else if (*c != -1) {
+            appendVector(line, c);
         }
     }
 
     if (line->len != 0) {
-        appendLine(data, line);
+        char *l = getLine(line);
+        appendVector(data, l);
     }
 
-    deleteVector(line);
+    deleteVector(line, false);
+
+    return data;
+}
+
+
+// Function to read single line input from a file, separated by delim
+struct vector *singleLine(char *fileName, char *delim) {
+    struct vector *data = createVector(stringsize, copyElement);
+
+    FILE *fp = fopen(fileName, "r");
+    if (!fp) {
+        return NULL;
+    }
+
+    struct vector *line = createVector(charsize, copyElement);
+
+    while (!feof(fp)) {
+        char *c = (char*)calloc(2, sizeof(char));
+        *c = fgetc(fp);
+        if (*c != -1) {
+            appendVector(line, c);
+        }
+    }
+
+    char *input_line = getLine(line);
+    deleteVector(line, false);
+
+    char *p = strtok(input_line, delim);
+    while (p) {
+        char *e = (char*)calloc(strlen(p), sizeof(char));
+        strcpy(e, p);
+        appendVector(data, e);
+        p = strtok(NULL, delim);
+    }
 
     return data;
 }
--- Modules/vector.h
+++ Modules/vector.h
@@ -2,40 +2,78 @@
 #define VECTOR_H
 
 
+// Include headers
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "adts.h"
 
 
-struct vector *createVector();
-void deleteVector(struct vector *v);
-void append(struct vector *v, void *e);
-void* pop(struct vector *v, int ix);
-struct vector *slice(struct vector *v, int start, int end, int step);
-struct vector *createCopy(struct vector *v, size_t (sizefun)(void *e));
+// Function Prototypes
+struct vector *createVector(size_t (*size)(void *e), void *(*copy)(void *e, size_t size));
+struct vector *createVectorSized(size_t (*size)(void *e), void *(*copy)(void *e, size_t size), int numEls);
+void deleteVector(struct vector *v, bool delEls);
+void appendVector(struct vector *v, void *e);
+void* popVector(struct vector *v);
+struct vector *sliceVector(struct vector *v, int start, int end, int step);
+bool inVector(struct vector *v, void *e, size_t size);
+int indexVector(struct vector *v, void *e, size_t size);
+void *createCopyVector(void *vec, size_t vecSize);
+size_t sizeofVector(void *e);
+struct vector *sortVector(struct vector *v, bool (*cmp)(void *e1, void *e2));
 
 
+// Declare Vector Struct
 struct vector {
     int len, cap;
+    size_t (*e_size)(void *e);  // Function to calculate size of an element
+    void *(*e_cpy)(void *e, size_t size);   // Function to create a copy of an element
     void **arr;
 };
 
 
-struct vector *createVector() {
+// Function to create a vector
+struct vector *createVector(size_t (*size)(void *e), void *(*copy)(void *e, size_t size)) {
     struct vector *v = (struct vector *)malloc(sizeof(struct vector));
     v->len = 0, v->cap = 1;
+    v->e_size = size;
+    v->e_cpy = copy;
     v->arr = (void**)calloc(1, sizeof(void*));
 
     return v;
 }
 
 
-void deleteVector(struct vector *v) {
+// Function to create vector of a certain size
+struct vector *createVectorSized(size_t (*size)(void *e), void *(*copy)(void *e, size_t size), int numEls) {
+    if (numEls > 0) {
+        struct vector *v = (struct vector *)malloc(sizeof(struct vector));
+        v->len = 0, v->cap = numEls;
+        v->e_size = size;
+        v->e_cpy = copy;
+        v->arr = (void**)calloc(numEls, sizeof(void*));
+
+        return v;
+    } else {
+        return NULL;
+    }
+}
+
+
+// Function to delete a vector
+void deleteVector(struct vector *v, bool delEls) {
+    if (delEls) {
+        for (int i = 0; i < v->len; i++) {
+            free(v->arr[i]);
+        }
+    }
     free(v->arr);
     free(v);
 }
 
 
-void append(struct vector *v, void *e) {
+// Function to append an element to a vector
+void appendVector(struct vector *v, void *e) {
     if (v->len == v->cap) {
         void **newArr = (void**)calloc(2 * v->cap, sizeof(void*));
         
@@ -53,57 +91,126 @@ void append(struct vector *v, void *e) {
 }
 
 
-void* pop(struct vector *v, int ix) {
-    if (ix >= v->len) {
-        exit(-1);
-    }
-
-    void *e = v->arr[ix];
-
+// Function to pop last element from a vector
+void* popVector(struct vector *v) {
+    void *e = v->arr[v->len - 1];
+    v->arr[v->len - 1] = NULL;
     v->len--;
 
     if (v->len <= v->cap / 4) {
+        void **newarr = (void**)calloc(v->cap, sizeof(void*));
+
+        for (int i = 0; i < v->len - 1; i++) {
+            newarr[i] = v->arr[i];
+        }
+
+        free(v->arr);
+        v->arr = newarr;
         v->cap /= 2;
     }
-
-    void **newarr = (void**)calloc(v->cap, sizeof(void*));
-
-    for (int i = 0; i <= v->len; i++) {
-        newarr[i] = v->arr[i + (i >= ix ? 1 : 0)];
-    }
-
-    free(v->arr);
-    v->arr = newarr;
 
     return e;
 }
 
 
-struct vector *slice(struct vector *v, int start, int end, int step) {
-    if ((start < 0 || start >= v->len) || (end < 0 || end >= v->len) || step == 0) {
+// Function to create a slice of a vector. Copies elements based on deepcopy
+struct vector *sliceVector(struct vector *v, int start, int end, int step) {
+    if ((start < 0 || start > v->len) || (end < 0 || end > v->len) || step == 0) {
         return NULL;
     }
 
-    struct vector *sliced = createVector();
+    struct vector *sliced = createVector(v->e_size, *v->e_cpy);
 
     for (int i = start; (i < end && step > 0) || (i > end && step < 0); i += step) {
-        append(sliced, v->arr[i]);
+        appendVector(sliced, v->arr[i]);
     }
 
     return sliced;
 }
 
 
-struct vector *createCopy(struct vector *v, size_t (sizefun)(void *e)) {
-    struct vector *copy = createVector();
+// Function to determine if an element is in a vector
+bool inVector(struct vector *v, void *e, size_t size) {
+    for (int i = 0; i < v->len; i++) {
+        if (v->e_size(v->arr[i]) == size && strncmp((char*)e, (char*)v->arr[i], size) == 0) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+
+// Function to get index of an element if it is in the vector
+int indexVector(struct vector *v, void *e, size_t size) {
+    for (int i = 0; i < v->len; i++) {
+        char *str = (char*)v->e_cpy(v->arr[i], v->e_size(v->arr[i]));
+        if (v->e_size(v->arr[i]) == size && strncmp((char*)e, str, size) == 0) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+
+// Creates a copy of vector
+void *createCopyVector(void *vec, size_t vecSize) {
+    struct vector *v = (struct vector *)vec;
+    struct vector *copy = createVector(v->e_size, *v->e_cpy);
 
     for (int i = 0; i < v->len; i++) {
-        void *newE = malloc(sizefun(v->arr[i]));
-        memcpy(newE, v->arr[i], sizefun(v->arr[i]));
-        append(copy, newE);
+        void *newE = v->e_cpy(v->arr[i], v->e_size(v->arr[i]));
+        appendVector(copy, newE);
     }
 
     return copy;
+}
+
+
+// Get size of vector type
+size_t sizeofVector(void *e) {
+    return sizeof(struct vector);
+}
+
+
+// Merge sorted vectors
+struct vector *mergeVec(struct vector *v1, struct vector *v2, bool (*cmp)(void *e1, void *e2)) {
+    struct vector *merged = createVector(v1->e_size, v1->e_cpy);
+    int i1 = 0, i2 = 0;
+    
+    while (i1 < v1->len && i2 < v2->len) {
+        void *e;
+        if (cmp(v1->arr[i1], v2->arr[i2])) {
+            e = v1->arr[i1];
+            i1++;
+        } else {
+            e = v2->arr[i2];
+            i2++;
+        }
+
+        appendVector(merged, e);
+    }
+
+    for (int i = i1; i < v1->len; i++) {
+        appendVector(merged, v1->arr[i]);
+    }
+
+    for (int i = i2; i < v2->len; i++) {
+        appendVector(merged, v2->arr[i]);
+    }
+
+    return merged;
+}
+
+
+// Sort a vector
+struct vector *sortVector(struct vector *v, bool (*cmp)(void *e1, void *e2)) {
+    if (v->len == 1) {
+        return v;
+    }
+
+    return mergeVec(sortVector(sliceVector(v, 0, v->len / 2, 1), cmp), sortVector(sliceVector(v, v->len / 2, v->len, 1), cmp), cmp);
 }
 
 
