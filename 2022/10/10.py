4b7b459a2093f236ac2db07806f1a0ef8c28c099
brandonhippe <bhippe@pdx.edu>
2022-12-09 21:31:11 -0800

    2022 Day 10 Finished

---
+++ 2022/10/10.py
@@ -0,0 +1,64 @@
+from time import perf_counter
+
+
+def main(filename):
+    with open(filename, encoding="UTF-8") as f:
+        lines = [line.strip('\n') for line in f.readlines()]
+
+    x = 1
+    cycles = 0
+
+    signalStrength = 0
+    for line in lines:
+        if line == "noop":
+            cycles += 1
+            if cycles in [20, 60, 100, 140, 180, 220]:
+                signalStrength += x * cycles
+        else:
+            cycles += 1
+            if cycles in [20, 60, 100, 140, 180, 220]:
+                signalStrength += x * cycles
+            
+            cycles += 1
+            if cycles in [20, 60, 100, 140, 180, 220]:
+                signalStrength += x * cycles
+
+            x += int(line.split(' ')[1])
+
+    print(f"\nPart 1:\nSum of signal strengths: {signalStrength}")
+    print(f"\nPart 2:\nText on CRT:")
+
+    x = 1
+    cycles = 0
+
+    for line in lines:
+        if cycles % 40 == 0:
+            print("")
+
+        if abs(x - (cycles % 40)) <= 1:
+            print("#", end='')
+        else:
+            print(' ', end='')
+
+        cycles += 1
+
+        if line != "noop":
+            if cycles % 40 == 0:
+                print("")
+
+            if abs(x - (cycles % 40)) <= 1:
+                print("#", end='')
+            else:
+                print(' ', end='')
+
+            x += int(line.split(' ')[1])
+
+            cycles += 1
+
+    print("")
+
+
+if __name__ == "__main__":
+    init_time = perf_counter()
+    main("input.txt")
+    print(f"\nRan in {perf_counter() - init_time} seconds.")
