de4534ece677a25c3397df676bc77b05c8bb8cd8
brandonhippe <bhippe@pdx.edu>
2022-12-12 11:47:43 -0800

    Part 2 done with BFS

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -42,11 +42,36 @@ def aStar(height, start, end):
     return float('inf')
 
 
+def bfs(height, end):
+    openList = [[end, 0]]
+    openSet = {end}
+    closedSet = set()
+
+    while len(openList) != 0:
+        pos, g = openList.pop(0)
+        openSet.remove(pos)
+
+        if height[pos] == 0:
+            return g
+
+        for noff in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+            nPos = tuple(p + n for p, n in zip(pos, noff))
+
+            if nPos not in height or height[pos] - height[nPos] > 1 or nPos in openSet or nPos in closedSet:
+                continue
+
+            openList.append([nPos, g + 1])
+            openSet.add(nPos)
+
+        closedSet.add(pos)
+
+
 def main(filename):
     with open(filename, encoding="UTF-8") as f:
         lines = [line.strip('\n') for line in f.readlines()]
 
     height = {}
+    lowPoints = set()
     for y, line in enumerate(lines):
         for x, l in enumerate(line):
             if l == "S":
@@ -59,13 +84,7 @@ def main(filename):
                 height[(x, y)] = ord(l) - ord('a')
 
     print(f"\nPart 1:\nFewest steps from start to summit: {aStar(height, start, end)}")
-
-    minLen = float('inf')
-    for pos, h in zip(height.keys(), height.values()):
-        if h == 0:
-            minLen = min(minLen, aStar(height, pos, end))
-
-    print(f"\nPart 2:\nShortest scenic path: {minLen}")
+    print(f"\nPart 2:\nShortest scenic path: {bfs(height, end)}")
 
 
 if __name__ == "__main__":
