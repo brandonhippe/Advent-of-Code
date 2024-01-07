4abb383aa01b3b5468b8119d477f0c200b9729cb
brandonhippe <bhippe@pdx.edu>
2024-01-07 14:21:42 -0800

    Added full year functionality to runner

--- runner.py
+++ runner.py
@@ -1,10 +1,35 @@
 import sys, os, importlib
 
 
-if __name__ == "__main__":
-    year, day = [int(n) for n in sys.argv[1:]]
+DONT_RUN = {(2016, 8), (2018, 10), (2019, 8), (2019, 11), (2019, 25), (2021, 13), (2022, 10)}
 
-    sys.path.append(os.path.join(os.getcwd(), str(year)))
-    code = importlib.import_module(f'{year}_{day}')
-    os.chdir(os.path.join(os.getcwd(), str(year)))
-    code.main(True)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        year, day = [int(n) for n in sys.argv[1:]]
+        days = [day]
+    elif len(sys.argv) == 2:
+        year = int(sys.argv[1])
+        days = [n + 1 for n in range(25)]
+    else:
+        raise ValueError("Invalid number of arguments passed")
+    
+    if len(days) > 1:
+        from Modules.progressbar import printProgressBar
+
+    totalTime = 0
+    currDir = os.getcwd()
+    for i, day in enumerate(days):
+        if len(days) > 1:
+            printProgressBar(i + 1, len(days))
+
+        sys.path.append(os.path.join(os.getcwd(), str(year)))
+        code = importlib.import_module(f'{year}_{day}')
+
+        os.chdir(os.path.join(os.getcwd(), str(year)))
+        (_, p1_elapsed), (_, p2_elapsed) = code.main(len(days) == 1)
+        os.chdir(currDir)
+
+        totalTime += p1_elapsed + p2_elapsed
+
+    print(f"\nTotal time: {totalTime:.4f} seconds")
