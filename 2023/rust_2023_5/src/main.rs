7c0736781351df2b7b3541c84bc0a419e58d33fd
brandonhippe <bhippe@pdx.edu>
2024-01-22 17:15:43 -0800

    2023 Day 5 in Rust

---
+++ 2023/rust_2023_5/src/main.rs
@@ -0,0 +1,193 @@
+use relative_path::RelativePath;
+use std::env;
+use std::fs;
+use std::time::Instant;
+use regex::Regex;
+use std::collections::HashMap;
+
+fn part1(contents: String) -> i64 {
+    let lines: Vec<String> = contents.lines().map(|line| {line.to_string()}).collect();
+    let re_num = Regex::new(r"(\d+)").unwrap();
+    let mut nums: Vec<(i64, i64)> = re_num
+        .captures_iter(&lines[0])
+        .map(|caps| {
+            let (_, [num]) = caps.extract();
+            (num.parse::<i64>().unwrap(), num.parse::<i64>().unwrap())
+        })
+        .collect();
+
+    let re_conv = Regex::new(r"^(\d+)\s+(\d+)\s+(\d+)$").unwrap();
+    let mut conversions: Vec<(i64, i64, i64)> = Vec::new();
+
+    for line in lines[2..lines.len()].iter() {
+        if line.len() == 0 {
+            continue;
+        }
+
+        let temp_conv: Option<(i64, i64, i64)> = re_conv
+            .captures(line)
+            .map(|caps| {
+                let (_, [t, f, d]) = caps.extract();
+                (t.parse::<i64>().unwrap(), f.parse::<i64>().unwrap(), d.parse::<i64>().unwrap())
+            });
+
+        if temp_conv.is_some() {
+            // Add conversion
+            conversions.push(temp_conv.unwrap());
+        } else {
+            // Convert the current nums
+            let mut new_nums = applyConversions(&conversions, &mut nums);
+            conversions.clear();
+            nums.append(&mut new_nums);
+        }
+    }
+
+    let mut new_nums = applyConversions(&conversions, &mut nums);
+    nums.append(&mut new_nums);
+
+    let mut minimum = nums[0].0;
+    for (start, _) in nums[1..nums.len()].iter() {
+        if *start < minimum {
+            minimum = *start;
+        }
+    }
+
+    return minimum;
+}
+
+fn part2(contents: String) -> i64 {
+    let lines: Vec<String> = contents.lines().map(|line| {line.to_string()}).collect();
+    let re_num = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    let mut nums: Vec<(i64, i64)> = re_num
+        .captures_iter(&lines[0])
+        .map(|caps| {
+            let (_, [s, e]) = caps.extract();
+            (s.parse::<i64>().unwrap(), s.parse::<i64>().unwrap() + e.parse::<i64>().unwrap() - 1)
+        })
+        .collect();
+
+    let re_conv = Regex::new(r"^(\d+)\s+(\d+)\s+(\d+)$").unwrap();
+    let mut conversions: Vec<(i64, i64, i64)> = Vec::new();
+
+    for line in lines[2..lines.len()].iter() {
+        if line.len() == 0 {
+            continue;
+        }
+
+        let temp_conv: Option<(i64, i64, i64)> = re_conv
+            .captures(line)
+            .map(|caps| {
+                let (_, [t, f, d]) = caps.extract();
+                (t.parse::<i64>().unwrap(), f.parse::<i64>().unwrap(), d.parse::<i64>().unwrap())
+            });
+
+        if temp_conv.is_some() {
+            // Add conversion
+            conversions.push(temp_conv.unwrap());
+        } else {
+            // Convert the current nums
+            let mut new_nums = applyConversions(&conversions, &mut nums);
+            conversions.clear();
+            nums.append(&mut new_nums);
+        }
+    }
+
+    let mut new_nums = applyConversions(&conversions, &mut nums);
+    nums.append(&mut new_nums);
+
+    let mut minimum = nums[0].0;
+    for (start, _) in nums[1..nums.len()].iter() {
+        if *start < minimum {
+            minimum = *start;
+        }
+    }
+
+    return minimum;
+}
+
+fn applyConversions(conversions: &Vec<(i64, i64, i64)>, nums: &mut Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    let mut new_nums: Vec<(i64, i64)> = Vec::new();
+    while nums.len() != 0 {
+        for i in (0..nums.len()).rev() {
+            let (start, end) = nums[i];
+            let mut split = false;
+
+            for (dest_start, src_start, d) in conversions {
+                if *src_start <= start && end < *src_start + *d {
+                    new_nums.push((convert(*dest_start, *src_start, start), convert(*dest_start, *src_start, end)));
+                    nums.remove(i);
+                    split = true;
+                    break;
+                } else if *src_start <= start && start < *src_start + *d {
+                    nums[i] = (start, *src_start + *d - 1);
+                    nums.push((*src_start + *d, end));
+                    split = true;
+                    break;
+                } else if *src_start <= end && end < *src_start + *d {
+                    nums[i] = (*src_start, end);
+                    nums.push((start, *src_start - 1));
+                    split = true;
+                    break;
+                }
+            }
+
+            if !split {
+                new_nums.push((start, end));
+                nums.remove(i);
+            }
+        }
+    }
+
+    return new_nums
+}
+
+fn convert(dest_start: i64, from_start: i64, num: i64) -> i64 {
+    return dest_start + (num - from_start);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1_test() {
+        let contents =
+            fs::read_to_string("example.txt").expect("Should have been able to read the file");
+
+        assert_eq!(part1(contents), 35);
+    }
+
+    #[test]
+    fn p2_test() {
+        let contents =
+            fs::read_to_string("example.txt").expect("Should have been able to read the file");
+
+        assert_eq!(part2(contents), 46);
+    }
+}
+
+fn main() {
+    let root = env::current_dir().unwrap();
+    let relative_path = if root.ends_with("rust_2023_5") {
+        RelativePath::new("../../Inputs/2023_5.txt")
+    } else {
+        RelativePath::new("/Inputs/2023_5.txt")
+    };
+
+    let contents = fs::read_to_string(relative_path.to_path(&root))
+        .expect("Should have been able to read the file");
+
+    let part1_timer = Instant::now();
+    println!(
+        "\nPart 1:\nLowest location number: {}\nRan in {:.5?}",
+        part1(contents.clone()),
+        part1_timer.elapsed()
+    );
+
+    let part2_timer = Instant::now();
+    println!(
+        "\nPart 2:\nLowest location number: {}\nRan in {:.5?}",
+        part2(contents.clone()),
+        part2_timer.elapsed()
+    );
+}
--- README.md
+++ README.md
@@ -21,4 +21,4 @@ Years with completed project(s) and languages:
 2022: 1-25 in Python
 
 2023: 1-25 in Python\
-      1-4 in Rust
+      1-5 in Rust
