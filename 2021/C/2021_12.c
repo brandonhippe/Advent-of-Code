cbd1ca366b4842e87cd5145f89bd45e6413bd57f
brandonhippe <bhippe@pdx.edu>
2024-02-29 14:13:25 -0800

    Format 2021_12.c

--- 2021/C/2021_12.c
+++ 2021/C/2021_12.c
@@ -62,8 +62,7 @@ int findPaths(struct dict *neighborData, struct dict *visitedData, char *node, b
     return paths;
 }
 
-
-int main () {
+int part1() {
     struct vector *input_data = multiLine(fileName);
     struct dict *neighborData = createDict(stringsize, sizeofVector, createCopyVector), *visitedData = createDict(stringsize, intsize, copyElement);
 
@@ -89,7 +88,52 @@ int main () {
         appendVector(p2Vec, p1);
     }
 
-    printf("\nPart 1:\nNumber of paths visiting small caves at most once: %d\n\nPart 2:\nNumber of paths visiting small caves at most twice: %d\n", findPaths(neighborData, visitedData, "start", false), findPaths(neighborData, visitedData, "start", true));
+    return findPaths(neighborData, visitedData, "start", false);
+}
 
-    return 1;
+int part2() {
+    struct vector *input_data = multiLine(fileName);
+    struct dict *neighborData = createDict(stringsize, sizeofVector, createCopyVector), *visitedData = createDict(stringsize, intsize, copyElement);
+
+    for (int i = 0; i < input_data->len; i++) {
+        char *line = (char*)calloc(strlen((char*)input_data->arr[i]), sizeof(char));
+        strcpy(line, input_data->arr[i]);
+
+        char *p1 = strtok(line, "-"), *p2 = strtok(NULL, "-");
+        
+        if (!inDict(neighborData, p1)) {
+            struct vector *nVec = createVector(stringsize, copyElement);
+            addDict(neighborData, p1, nVec);
+        }
+
+        if (!inDict(neighborData, p2)) {
+            struct vector *nVec = createVector(stringsize, copyElement);
+            addDict(neighborData, p2, nVec);
+        }
+        
+        struct vector *p1Vec = (struct vector *)getDictVal(neighborData, p1), *p2Vec = (struct vector *)getDictVal(neighborData, p2);
+        
+        appendVector(p1Vec, p2);
+        appendVector(p2Vec, p1);
+    }
+
+    return findPaths(neighborData, visitedData, "start", true);
+}
+
+
+int main () {
+    clock_t t;
+    t = clock(); 
+    int p1 = part1();
+    t = clock() - t; 
+    double t_p1 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 1:\nNumber of paths visiting small caves at most once: %d\nRan in %f seconds\n", p1, t_p1);
+
+    t = clock(); 
+    int p2 = part2();
+    t = clock() - t;
+    double t_p2 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 2:\nNumber of paths visiting small caves at most twice: %d\nRan in %f seconds\n", p2, t_p2);
+
+    return 0;
 }
