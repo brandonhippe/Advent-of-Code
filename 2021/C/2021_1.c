d4b87c9fe3fc56f1427a29d82ab45f64a750b443
brandonhippe <bhippe@pdx.edu>
2024-02-29 13:07:39 -0800

    Format 2021_1.c

--- 2021/C/2021_1.c
+++ 2021/C/2021_1.c
@@ -8,7 +8,7 @@
 #include "../../Modules/vector.h"
 #define fileName "../../Inputs/2021_1.txt"
 
-int main () {
+int part1() {
     struct vector *input_data = multiLine(fileName);
     struct vector *data = createVector(intsize, copyElement);
 
@@ -23,22 +23,46 @@ int main () {
         increase_count += (*(int*)data->arr[i] > *(int*)data->arr[i - 1]) ? 1 : 0;
     }
 
-    printf("\nPart 1:\nNumber of measurements larger than the previous element: %d\n", increase_count);
-    
+    return increase_count;
+}
+
+int part2() {
+    struct vector *input_data = multiLine(fileName);
+    struct vector *data = createVector(intsize, copyElement);
+
+    for (int i = 0; i < input_data->len; i++) {
+        int *val = (int*)calloc(1, sizeof(int));
+        *(val) = atoi((char*)input_data->arr[i]);
+        appendVector(data, val);
+    }
 
     struct vector *sum1 = sliceVector(data, 0, data->len - 2, 1);
     struct vector *sum2 = sliceVector(data, 1, data->len - 1, 1);
     struct vector *sum3 = sliceVector(data, 2, data->len, 1);
     
-    increase_count = 0;
+    int increase_count = 0;
     for (int i = 1; i < sum1->len; i++) {
         int val = *(int*)sum1->arr[i] + *(int*)sum2->arr[i] + *(int*)sum3->arr[i];
         int pval = *(int*)sum1->arr[i - 1] + *(int*)sum2->arr[i - 1] + *(int*)sum3->arr[i - 1];
         increase_count += (val > pval) ? 1 : 0;
     }
 
-    printf("\nPart 2:\nNumber of windows larger than previous window: %d\n", increase_count);
-    
+    return increase_count;
+}
 
-    return 1;
+int main () {
+    clock_t t;
+    t = clock(); 
+    int p1 = part1();
+    t = clock() - t; 
+    double t_p1 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 1:\nNumber of measurements larger than the previous element: %d\nRan in %f seconds\n", p1, t_p1);
+    
+    t = clock(); 
+    int p2 = part2();
+    t = clock() - t;
+    double t_p2 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 2:\nNumber of windows larger than previous window: %d\nRan in %f seconds\n", p2, t_p2);    
+
+    return 0;
 }
