9608126c89cceb95fd4992f406c271c00525426d
brandonhippe <bhippe@pdx.edu>
2024-02-29 18:00:38 -0800

    Add C template

---
+++ Templates/template.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+#define fileName "../../Inputs/{year}_{day}.txt"
+
+int part1() {
+    return 0;
+}
+
+int part2() {
+    return 0;
+}
+
+int main () {
+    clock_t t;
+    t = clock(); 
+    int p1 = part1();
+    t = clock() - t; 
+    double t_p1 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 1:\n%d\nRan in %f seconds\n", p1, t_p1);
+
+    t = clock(); 
+    int p2 = part2();
+    t = clock() - t;
+    double t_p2 = ((double)t) / CLOCKS_PER_SEC;
+    printf("\nPart 2:\n%d\nRan in %f seconds\n", p2, t_p2);
+
+    return 0;
+}
